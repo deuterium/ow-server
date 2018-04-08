5e14efa6ddda38eb4259ca19164405ff71cb3f98
Chris Wood <chriswood.ca@gmail.com>
2018-04-08 13:23:33 -0700

    renamed hello.py to main.py, updated gitignore

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import io, json, time
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'main page'
+
+@app.route('/api/client/submit', methods=['POST']) 
+def foo():
+    if not request.json:
+        abort(400)
+    with io.open('clientdata/json.txt', 'a', encoding='utf-8') as f:
+        f.write("%s\n" % json.dumps(request.get_json(), ensure_ascii=False))
+    return jsonify(request.get_json())
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=80)
