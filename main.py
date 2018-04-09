3a84d84863b356419c7d32ca18ae94310a6e3e11
Chris Wood <chriswood.ca@gmail.com>
2018-04-08 19:23:52 -0700

    ignored idea files, updated to python3 writing

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-from flask import Flask, request, jsonify
-import io, json, time
+from flask import Flask, request, jsonify, abort
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-	with io.open('clientdata/json.txt', 'r', encoding='utf-8') as f:
-		return f.read()
+    with open('clientdata/json.txt', 'r', encoding='utf-8') as f:
+        return f.read()
 
-@app.route('/api/client/submit', methods=['POST']) 
+
+@app.route('/api/client/submit', methods=['POST'])
 def foo():
     if not request.json:
         abort(400)
     with io.open('clientdata/json.txt', 'a', encoding='utf-8') as f:
         f.write("%s\n" % json.dumps(request.get_json(), ensure_ascii=False))
     return jsonify(request.get_json())
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
