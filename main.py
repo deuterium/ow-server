3c65b0efcf8a75e1abbb7bbf0733f1aeae4e7220
Chris Wood <chriswood.ca@gmail.com>
2018-06-03 09:37:04 -0700

    started base layout and a few page inheriting from that

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-from flask import Flask, request, jsonify, abort
-import json
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    with open('clientdata/json.txt', 'r', encoding='utf-8') as f:
-        return f.read()
+    return render_template("test.html")
 
-
-@app.route('/api/client/submit', methods=['POST'])
-def foo():
-    if not request.json:
-        abort(400)
-    with open('clientdata/json.txt', 'a', encoding='utf-8') as f:
-        f.write("%s\n" % json.dumps(request.get_json(), ensure_ascii=False))
-    return jsonify(request.get_json())
-
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=8080)
