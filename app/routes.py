a3d6dc9aacab01489ee5f6f412d0a3b1eb2dc688
Chris Wood <chriswood.ca@gmail.com>
2018-08-12 15:26:57 -0700

    started work on login page/authentication

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,37 @@
 from app import app
-from flask import render_template, url_for, flash, get_flashed_messages, request, redirect, abort, session
+from functools import wraps
+from flask import render_template, url_for, flash, get_flashed_messages, request, redirect, abort, session, g
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not ValidToken(session.get('loginToken')):
+            #redirect to login page
+            print("login_required decorater")
+        return f(*args, **kwargs)
+    return decorated_function
+
+def ValidToken(t):
+    if t is None:
+        print("BLANK TOKEN")
+        return False
+    elif t is "testtoken":
+        print("GOOD TOKEN")
+        return True
+    else:
+        print("BAD TOKEN")
+        return False
+
+# # Create a user to test with
+# @app.before_first_request
+# def create_user():
 
 @app.route('/')
 def hello():
     return render_template("test.html")
 
 @app.route('/about')
+@login_required
 def about():
     return render_template("about.html")
+
